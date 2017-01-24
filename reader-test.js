ff35a8897361198a8d0d57a7863efb64ccbe7f82
Benj Sicam <benj@codechef.io>
2017-01-24 13:16:38 +0800

    Fixes

--- reader-test.js
+++ reader-test.js
@@ -24,7 +24,7 @@ SerialPort.list(function (err, ports) {
 		rfIdPort.on('data', dataListener);
 
 		rfIdPort.once('disconnect', function () {
-			console.log(`${rfIdPort.comName} port has been closed/disconnected.`);
+			console.log(`${port.comName} port has been closed/disconnected.`);
 
 			rfIdPort.removeListener('data', dataListener);
 		});
@@ -32,18 +32,19 @@ SerialPort.list(function (err, ports) {
 		rfIdPort.open(function (err) {
 			if (err) console.error(err);
 
-			console.log(`${rfIdPort.comName} port has been opened.`);
+			console.log(`${port.comName} port has been opened.`);
 
 			// Flush all inputs
 			rfIdPort.flush(function (err) {
 				if (err) console.error(err);
 
-				console.log(`${rfIdPort.comName} inputs have been flushed.`);
+				console.log(`${port.comName} inputs have been flushed.`);
 				// Write this byte sequence to start reading
-				rfIdPort.write(new Buffer([0x04, 0x00, 0x01, 0xDB, 0x4B]), function (err) {
+				//rfIdPort.write(new Buffer([0x04, 0x00, 0x01, 0xDB, 0x4B]), function (err) {
+				rfIdPort.write(new Buffer('\x04\x00\x01\xDB\x4B'), function (err) {
 					if (err) console.error(err);
 
-					console.log(`${rfIdPort.comName} bytes have been written.`);
+					console.log(`${port.comName} bytes have been written.`);
 				});
 			});
 		});
